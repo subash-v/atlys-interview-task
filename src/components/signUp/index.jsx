6f3135827d88c0334b9ca8e57d7c5cf8771b5eab
Subash V <42377150+subash-v@users.noreply.github.com>
2023-09-04 22:04:25 +0530

    Update index.jsx

--- src/components/signUp/index.jsx
+++ src/components/signUp/index.jsx
@@ -70,7 +70,7 @@ export default function index() {
                   required={true}
                 />
               </div>
-
+              <Link to='/feed'>
               <button
                 type="submit"
                 className="w-full text-white bg-[#4A96FF] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
@@ -78,6 +78,7 @@ export default function index() {
               >
                 Continue
               </button>
+              </Link>
               <p className="text-sm font-light text-[#7F8084] ">
               Already have an account?{" "} 
               <Link to='/'>
